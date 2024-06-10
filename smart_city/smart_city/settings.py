bb63fe8e2be5ebb417f331667bca5d18168b9158
Isabelli Marques <isabellimarques14@gmail.com>
2024-06-10 10:53:45 -0300

    update para web

--- smart_city/smart_city/settings.py
+++ smart_city/smart_city/settings.py
@@ -145,7 +145,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOW_ALL_ORIGINS = True 
 
-# CORS_ALLOWED_ORIGINS = [
-#    "http://localhost:5173",
+CORS_ALLOWED_ORIGINS = [
+   "http://localhost:5173",
    
-# ]
+]
